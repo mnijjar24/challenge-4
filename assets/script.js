69a1b5ebccf953b91c8ea0690fc832b093d7916f
michelle <michellenijjar@hotmail.com>
2021-10-10 16:07:53 -0400

    second commit

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+var highScoreArray= JSON.parse(localStorage.getItem("highScoreArray")) || []   
+console.log(highScoreArray);
 var timeEl = document.createElement("h3");
 var startButton = document.getElementById("start-button");
 var sameGameEl = document.querySelector(".start-game");
@@ -5,61 +7,67 @@ timeEl.setAttribute("id", "timeEl");
 var time = 60;
 var questionCard = document.querySelector(".question-card")
 
-console.log(timeEl);
+//Start Game Function
 function startGame() {
     sameGameEl.classList.add("hide");
     questionCard.classList.remove("hide");
+    createCard()
 }
+//Named the timeInterval so have a variable to use for clear interval. (Timer)
 var timeInterval = setInterval(function () {
     time--;
     if (time < 0) {
         time = 0
     }
 
-    timeEl.textContent = "timer" + time;
+    timeEl.textContent = time;
 }, 1000)
 
-
+//Questions
 var questions = [
     {
-        questionText: "1. What does HTML stand for?",
-        choices: ["a", "b", "c"],
-        answer: "a"
+        questionText: "1. An HTML document can contain _____",
+        choices: ["a. Attributes", "b. Tags ", "c. Raw text", "d. All of the above"],
+        answer: "d. All of the above"
     },
 
     {
-        questionText: "question-2",
-        choices: ["a", "b", "c"],
-        answer: "a"
+        questionText: "2. A page designed in HTML is called _____",
+        choices: ["a. Application", "b. Cover page", "c. Front-end", "d. Web Page"],
+        answer: "d. Web Page"
     },
 
     {
-        questionText: "question-3",
-        choices: ["a", "b", "c"],
-        answer: "c"
+        questionText: "3. If we want to place text around an image, which CSS property should we use?",
+        choices: ["a. Push", "b. Float", "c. Align", "d. Wrap"],
+        answer: "b. Float"
     },
 
     {
-        questionText: "question-4",
-        choices: ["a", "b", "c"],
-        answer: "b"
+        questionText: "4. Can we align an element by setting margin-left and margin-right?",
+        choices: ["a. Yes, it is possible", "b. No, it is not possible"],
+        answer: "b. No, it is not possible"
     },
 
     {
-        questionText: "question-5",
-        choices: ["a", "b", "c"],
-        answer: "c"
+        questionText: "5. Choose the correct HTML tag for a large title.",
+        choices: ["a. H1", "b. Heading", "c. Head", "d. H6"],
+        answer: "a. H1"
     },
 ];
 
 var score = 0;
 
-var qi = 0
+var qi = 0;
+//Function that builds the question that runs each time we run the function
 function createCard() {
     questionCard.innerHTML = ""
     var h2element = document.createElement('h2');
     var buttonBox = document.createElement("div");
+    buttonBox.setAttribute("class","button-box");
     h2element.textContent = questions[qi].questionText;
+
+    //Loops over choices array and creates a button for each choice
     questions[qi].choices.forEach(function (choice) {
         var btnEl = document.createElement("button");
         btnEl.textContent = choice;
@@ -67,19 +75,23 @@ function createCard() {
         btnEl.onclick = checkAnswer;
         buttonBox.appendChild(btnEl);
     });
+    //Help with spacing and relative positioning
     var spanEl = document.createElement("span");
     spanEl.appendChild(timeEl);
+
+    //Appending question elements to the page
     questionCard.appendChild(spanEl);
     questionCard.appendChild(h2element);
     questionCard.appendChild(buttonBox);
 }
-createCard()
+
+//Tells if right/wrong
 function checkAnswer() {
     console.log(this.value);
     if (this.value !== questions[qi].answer) {
         console.log("wrong");
         time -= 10;
-        timeEl.textContent = "timer" + time;
+        timeEl.textContent = time;
         if (time <= 0) {
             endgame()
         }
@@ -87,8 +99,11 @@ function checkAnswer() {
     else {
         console.log("right");
         score++;
-        console.log(score);
-    } qi++;
+        console.log(score); 
+    } 
+    // Increase index (start at 0 and keep moving up) to move to next question
+    qi++;
+    // Checks returning questions, first one will end the game and second will build a card (moves to next question)
     if (qi === questions.length) {
 
         endgame()
@@ -97,7 +112,7 @@ function checkAnswer() {
     }
 }
 
-
+// End game functionality
 function endgame() {
     questionCard.setAttribute("style", "display: none");
     clearInterval(timeInterval);
@@ -105,16 +120,35 @@ function endgame() {
     buildEndScreen()
 }
 
+//End game screen 
 function buildEndScreen() {
+    var totalScore = score * time;
+    console.log(totalScore);
     var main = document.querySelector(".main");
+    var label = document.createElement("label");
+    label.textContent = "initials";
+    label.setAttribute("for", "initials");
+    var input = document.createElement("input");
+    input.setAttribute("type","text");
     var enddiv = document.createElement("div");
-    enddiv.innerHTML = "<h3>endgame" + score + "</h3>";
+    var highScore = {
+        name: "cqy",
+        score: totalScore
+    }
+   highScoreArray.push(highScore);
+   localStorage.setItem("highScoreArray",JSON.stringify(highScoreArray));
+   console.log(localStorage);
+    enddiv.innerHTML = "<h3>endgame " + totalScore + "</h3>";
     enddiv.setAttribute("class", "end-game");
+    enddiv.appendChild(label);
+    enddiv.appendChild(input);
     main.appendChild(enddiv);
 }
-
+function getHighScore() {
+    //retrieve scores from local storage
+}
+ 
 var container = document.querySelector('body, timeEl');
 var quest = document.querySelector('body questionCard');
-// container.insertBefore(newDiv, quest);
 
 startButton.addEventListener("click", startGame);
